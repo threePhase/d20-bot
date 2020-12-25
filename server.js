dd615ae8841802cdeda8a2fc085f14022107b04f
Michael Snider <snidemic@gmail.com>
2020-12-25 10:12:43 -0800

    Lookup emoji by name
    Lookup emojis by their name in the discord client, instead of using the
    colon formatting, which is not directly supported when replying as a
    bot.

--- server.js
+++ server.js
@@ -13,33 +13,33 @@ const roll = ({d}) => {
 }
 const d20Map = [
   null, // unused - rolls range from indicies 1-20
-  ':d201:',
-  ':d202:',
-  ':d203:',
-  ':d204:',
-  ':d205:',
-  ':d206:',
-  ':d207:',
-  ':d208:',
-  ':d209:',
-  ':d2010:',
-  ':d2011:',
-  ':d2012:',
-  ':d2013:',
-  ':d2014:',
-  ':d2015:',
-  ':d2016:',
-  ':d2017:',
-  ':d2018:',
-  ':d2019:',
-  ':d2020:',
+  'd201',
+  'd202',
+  'd203',
+  'd204',
+  'd205',
+  'd206',
+  'd207',
+  'd208',
+  'd209',
+  'd2010',
+  'd2011',
+  'd2012',
+  'd2013',
+  'd2014',
+  'd2015',
+  'd2016',
+  'd2017',
+  'd2018',
+  'd2019',
+  'd2020',
 ];
 
 const fateMap = [
   null, // unused - rolls range from indicies 1-3
-  ':dfneg:',
-  ':df:',
-  ':dfplus:',
+  'dfneg',
+  'df',
+  'dfplus',
 ];
 
 client.on('ready', () => {
@@ -50,14 +50,16 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === '!d20') {
     const idx = roll({ d: 20 });
-    msg.reply(d20Map[idx]);
+    const emoji = client.emojis.cache.find((emoji) => emoji.name === d20Map[idx]);
+    msg.reply(`${emoji}`);
   }
   if (msg.content === '!4dF') {
     const values = Array.from({ length: 4 }, () => roll({ d: 3 }));
     const sum = values.reduce((acc, val) =>
       // subtract 2 from all values to get range from -1 to +1
       acc + val - 2, 0);
-    const rolls = values.map((v) => fateMap[v]);
+    const rolls = values.map((v) =>
+      client.emojis.cache.find((emoji) => emoji.name === fateMap[v]));
     msg.reply(`${rolls}: ${sum}`);
   }
 });
